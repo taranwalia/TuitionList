3f283cc3b79bfa11d43a3d41d8a083b450a4f4f7
Taran Walia <114566741+taranwalia@users.noreply.github.com>
2026-07-13 07:28:27 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
   return (
     <html lang="en-GB">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-5873221361215755" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5873221361215755"
